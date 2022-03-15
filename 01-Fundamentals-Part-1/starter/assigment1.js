f0fe4a4a54a3f76ae379f847bdba4659e598e1fb
Brenda Aracelli <brendaracelli1997@gmail.com>
2022-03-14 23:51:01 -0500

    exercises 01

---
+++ 01-Fundamentals-Part-1/starter/assigment1.js
@@ -0,0 +1,7 @@
+let country = 'peru';
+let continent = 'America';
+let population = 8000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/scriptexample.js
@@ -0,0 +1,115 @@
+/*
+let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let FirstName = 'Matilda';
+
+console.log(FirstName);
+console.log(FirstName);
+console.log(FirstName);
+
+//Variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1900;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Aracelli';
+console.log(lastName);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 **3 es decir 2 a la exponencial de 3
+
+const firstName = 'Jonas';
+const lastName = 'Aracelli';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4 // x times equals 4
+// x = x * 4 = 25 * 4 = 100
+x++ // x = x + 1 = 100 + 1 = 101
+x-- // x = x -1 = 101 - 1 = 100
+x-- // 99
+console.log(x);
+
+console.log(ageJonas > ageSarah); // > < => =<
+// asking the operator is sgejonas greater than ageSarah
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+//es lo mismo que ageJonas > a ageSarah
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+// porque primero resta y luego compara?
+// js has a well defined order of operator precedence
+// order of how operator are executed
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5 // x = y = 10
+// de derecha a ziquireda 10 = y, y = x, entonce x = 10
+
+
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+// usamos parentesis porque el operator precedence
+// tiene a la division primero que la suma
+console.log(ageJonas, ageSarah); 
+*/
